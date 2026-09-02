2ba795071c4b2d08eff2613b72fc045a8c33b3a3
Joshua Masih <joshuafilm44@gmail.com>
2026-09-02 06:39:29 -0600

    Initial Commit

---
+++ Bolza/BolzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BolzaApp.swift
+//  Bolza
+//
+//  Created by Joshua Masih on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BolzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
